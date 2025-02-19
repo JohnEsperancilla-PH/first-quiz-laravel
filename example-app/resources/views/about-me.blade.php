02574cc73f3f198f2825dda948823ddcf57bd814
JOHN LEONARD TAYAPAD ESPERANCILLA <2010011@cc.usls.edu>
2025-02-19 13:55:54 +0800

    About Me Page

---
+++ example-app/resources/views/about-me.blade.php
@@ -0,0 +1,6 @@
+@include ('header')
+
+<h1>About me</h1>
+<h2>Hi, I am John Esperancilla</h2>
+
+@include ('footer')
---
+++ example-app/resources/views/header.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <title>Jan</title>
+    </head>
+
+<h1>Header</h1>
+
